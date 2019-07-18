0a521f48e562764a4751d19e88726b11f221f58a
Kees Schotanus <kees.schotanus@planet.nl>
2019-07-18 19:43:07 +0200

    Dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,49 @@
 self.addEventListener('install', event => {
-    console.log('[Service Worker] Installing ...', event)
-    event.waitUntil(caches.open('static')
-        .then(cache => {
-            console.log('[Service Worker] Pre-caching App Shell');
-            cache.addAll([
-                '/',
-                '/index.html',
-                '/src/js/app.js',
-                '/src/js/feed.js',
-                '/src/js/promise.js',
-                '/src/js/fetch.js',
-                '/src/js/material.min.js',
-                '/src/css/app.css',
-                '/src/css/feed.css',
-                '/src/images/main-image.jpg',
-                'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                'https://fonts.googleapis.com/icon?family=Material+Icons',
-                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-            ]);
-        })
-    )
+  console.log('[Service Worker] Installing ...', event)
+  event.waitUntil(caches.open('static')
+    .then(cache => {
+      console.log('[Service Worker] Pre-caching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
+    })
+  )
 });
 
 self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activating ...', event);
-    return self.clients.claim();
+  console.log('[Service Worker] Activating ...', event);
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => {
-        if (response) {
-            return response;
-        } else {
-          return fetch(event.request);
-        }
-    }))
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(res => {
+          return caches.open('dynamic')
+            .then(cache => {
+              cache.put(event.request.url, res.clone());
+              return res;
+            })
+            .catch(err => {
+
+            })
+        });
+      }
+    })
+  );
 });

b1ba9bb1765ae07b416a752cb6ee9507ec5b316a
Kees Schotanus <kees.schotanus@planet.nl>
2019-07-20 20:32:14 +0200

    Now uses versioned caching.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
+const STATIC_CACHE_NAME = 'static-v3';
+const DYNAMIC_CACHE_NAME = 'dynamic-v3'; 
+
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing ...', event)
-  event.waitUntil(caches.open('static')
+  event.waitUntil(caches.open(STATIC_CACHE_NAME)
     .then(cache => {
       console.log('[Service Worker] Pre-caching App Shell');
       cache.addAll([
@@ -24,6 +27,17 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating ...', event);
+  event.waitUntil(
+    caches.keys()
+      .then(keyList => {
+        return Promise.all(keyList.map(key => {
+          if (key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 
@@ -34,7 +48,7 @@ self.addEventListener('fetch', event => {
         return response;
       } else {
         return fetch(event.request).then(res => {
-          return caches.open('dynamic')
+          return caches.open(DYNAMIC_CACHE_NAME)
             .then(cache => {
               cache.put(event.request.url, res.clone());
               return res;

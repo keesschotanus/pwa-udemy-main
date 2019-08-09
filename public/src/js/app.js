66a619dd576f7c555ecafe5e7b242fb5e73784d7
Kees-Schotanus <Kees.schotanus@iddinkgroup.com>
2019-08-09 14:17:54 +0200

    Subscriptions stored in firebase

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -25,10 +25,25 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 function displayConfirmNotification() {
-  const options = {
-    body: 'You successfully subscribed to our service!'
-  };
-  new Notification('Successfully subscribed', options);
+  if ('serviceWorker' in navigator) {
+    const options = {
+      body: 'You successfully subscribed to our service!',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      actions: [
+        { action: 'confirm', title: 'Okay', icon: '/src/image/icons/app-icon-96x96.png' },
+        { action: 'cancel', title: 'Cancel', icon: '/src/image/icons/app-icon-96x96.png' },
+      ],
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true
+    };
+
+    navigator.serviceWorker.ready
+      .then(swreg =>{
+        swreg.showNotification('Successfully subscribed', options);
+      })
+  }
 }
 
 function askForNotificationPermission() {
@@ -37,12 +52,53 @@ function askForNotificationPermission() {
     if (result !== 'granted') {
       console.log('User did not grant permission');
     } else {
-      displayConfirmNotification();
+      configurePushSub();
     }
   });
 }
 
-if ('Notification' in window) {
+function configurePushSub() {
+  var reg;
+  navigator.serviceWorker.ready
+    .then(swreg => {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(sub => {
+      if (sub) {
+        // Use existing subscription
+      } else {
+        // Create a new subscription
+        var vapidPublicKey = 'BGKUmnOytLjQkNUJvI6tGPY1KmuJ5JG5J2yPO8KWhouDaVe2nxDR1d3oV0J62jPTzpyrLn7ehx667PQ3VDCP1VY';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey          
+        });
+      }
+    })
+    .then(newSub => {
+      // Pass new subscription to firebase backend
+      return fetch('https://udemy-pwa-bc405.firebaseio.com/subscriptions.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(newSub)
+      })
+    })
+    .then(res => {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; ++i) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
--- public/sw.js
+++ public/sw.js
@@ -218,3 +218,20 @@ self.addEventListener('sync', event => {
     )
   }
 })
+
+self.addEventListener('notificationClick', event => {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+  if (action === 'confirm') {
+    console.log('Confirm was chosen!')
+    notification.close();
+  } else {
+    console.log(action);
+  }
+})
+
+self.addEventListener('notificationclose', event => {
+  console.log('Notification was closed', event);
+})

167e62ec6f3868589d6209936f108e78700b0e5a
Kees-Schotanus <Kees.schotanus@iddinkgroup.com>
2019-08-12 15:40:17 +0200

    Sending push messages from server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,7 @@ import * as functions from 'firebase-functions';
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 const fs = require('fs');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebasevar!");
-// });
+const webpush = require('web-push');
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(fs.readFileSync("./udemy-pwa-firebase.json"))),
@@ -18,17 +12,40 @@ admin.initializeApp({
 exports.storePostData = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     admin.database().ref('posts').push({
-        id: request.body.id,
-        title: request.body.title,
-        location: request.body.location,
-        image: request.body.image,
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image,
     })
     .then(() => {
-        response.status(201).json({ message: 'Data stored', id: request.body.id}) 
+      webpush.setVapidDetails(
+        'mailto:kees.schotanus@planet.nl',
+        'pubBGKUmnOytLjQkNUJvI6tGPY1KmuJ5JG5J2yPO8KWhouDaVe2nxDR1d3oV0J62jPTzpyrLn7ehx667PQ3VDCP1VYkey',
+        'private key');
+      return admin.database().ref('subscriptions').once('value');
     })
-    .catch((err: any) => {
-        response.status(500).json({error: err})
-    });
+    .then((subscriptions: any) => {
+      subscriptions.forEach((subscription: any) => {
+        const pushConfig = {
+          endpoint: subscription.val().endpoint,
+          keys: {
+            auth: subscription.val().keys.auth,
+            p256dh: subscription.val().p256dh
+          }
+        }
+        webpush.sendNotification(pushConfig, JSON.stringify({
+          title: "New Post",
+          content: 'New Post added!'
+        }))
+        .catch((err: any) => {
+          console.log(err);
+        })
+      });
+      response.status(201).json({ message: 'Data stored', id: request.body.id }) 
+    })
+  })
+  .catch((err: any) => {
+    response.status(500).json({error: err})
   });
 });
 
